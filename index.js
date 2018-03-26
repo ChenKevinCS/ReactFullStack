721d12d91c7740ea2c74e3f52078855ba9b1c078
ChenKevinCSgitcommit -m testgit config --global user.name ChenKevinCS <chenkevincs@gmail.com>
2018-03-27 02:24:28 +0800

    make it b334r

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const passport = require('passport');
 const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
