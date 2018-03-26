3f5aa8a648c796f156ffdce024c32e9c35d6ca45
ChenKevinCSgitcommit -m testgit config --global user.name ChenKevinCS <chenkevincs@gmail.com>
2018-03-27 00:15:00 +0800

    v1

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,13 @@
+// SurveyNew shows SurveyForm and SurveyFormReview
+import React from 'react';
+
+const SurveyNew = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Emaily!</h1>
+      Collect feedback form your users
+    </div>
+  );
+};
+
+export default SurveyNew;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,13 +8,20 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
+
   app.get('/api/current_user', (req, res) => {
-    req.send(req.user);
+    res.send(req.user);
   });
 };
