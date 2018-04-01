a0daa6400f33f1bcf9265fed2801514c41aa04cb
ChenKevinCSgitcommit -m testgit config --global user.name ChenKevinCS <chenkevincs@gmail.com>
2018-04-01 23:01:57 +0800

    finished

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Emaily!</h1>
+      <h1>
+        Emaily!
+      </h1>
       Collect feedback form your users
     </div>
   );
